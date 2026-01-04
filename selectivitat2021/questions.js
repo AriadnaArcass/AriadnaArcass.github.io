c59757928513831b8365a22b3191c114d2622bb2
AriadnaArcass <aarcas4@iespompeufabra.cat>
2026-01-04 17:06:04 +0100

    Update questions.js

--- selectivitat2021/questions.js
+++ selectivitat2021/questions.js
@@ -201,28 +201,25 @@ images: `<img src="ruta/a/imatgePregunta.jpg" alt="Imatge relacionada amb la pre
         <br>
         <strong>b)Funció lògica</strong>
         \\[
-        t = (m_1 + m_2 + p_1 + p_2) \\cdot (m_1 + m_2 + p_1 + \\( \\bar{p}_2 \\)) \\cdot (m_1 + m_2 + \\( \\bar{p}_1 \\) + p_2)
+        t = (m_1 + m_2 + p_1 + p_2) \\cdot (m_1 + m_2 + p_1 + \\(bar{p}_2 \\)) \\cdot (m_1 + m_2 + \\(bar{p}_1 \\) + p_2)
         \\]
         \\[
-        t = (m_1 + m_2 + \\( \\bar{p}_1 \\) + \\( \\bar{p}_2 \\)) \\cdot (m_1 + \\( \\bar{m}_2 \\) + p_1 + p_2)
+        t = (m_1 + m_2 + \\(bar{p}_1\\) + \\(bar{p}_2 \\)) \\cdot (m_1 + \\(bar{m}_2 \\) + p_1 + p_2)
         \\]
         \\[
-        t = (\\( \\bar{m}_1 \\) + m_2 + p_1 + p_2) \\cdot (\\( \\bar{m}_1 \\) + \\( \\bar{m}_2 \\) + p_1 + p_2)
+        t = (\\(bar{m}_1 \\) + m_2 + p_1 + p_2) \\cdot (\\(bar{m}_1 \\) + \\(bar{m}_2 \\) + p_1 + p_2)
         \\]
         <p>Simplifiquem:</p>
         \\[
         t = (m_1 + m_2) \\cdot (p_1 + p_2)
         \\]
         <strong>c)Esquema de portes lògiques</strong>
-
-m1 ──┐
-     ├─ OR ──┐
-m2 ──┘       │
-             ├─ AND ── t
-p1 ──┐       │
-     ├─ OR ──┘
-p2 ──┘
-
+        function circuit(m1, m2, p1, p2) {
+        const orM = m1 || m2;   // OR entre m1 i m2
+        const orP = p1 || p2;   // OR entre p1 i p2
+        const t = orM && orP;   // AND final
+        return t;
+        }
            <br><br>
             <img src="ruta/a/imatgeResposta.jpg" alt="Imatge relacionada amb la resposta">`,  // Imatge opcional al final de la resposta
 images: `<img src="ruta/a/imatgePregunta.jpg" alt="Imatge relacionada amb la pregunta">`  // Imatge opcional al final de la pregunta
